b34fa60526b869e97c83334f666ca296e06cbaca
Sawsdevx8 <sawsdevx8@gmail.com>
2025-11-20 08:14:37 -0500

    Feat: Add proyect base execution

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"github.com/sawsdev/github-user-activity/cmd"
+)
+
+func main(){
+	if err := cmd.RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
