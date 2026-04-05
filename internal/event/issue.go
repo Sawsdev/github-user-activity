43752de1c40b6409bb261485b89f08745ee4c8ed
Sawsdevx8 <sawsdevx8@gmail.com>
2026-04-05 16:36:47 -0500

    Feat: Add issue struct

---
+++ internal/event/issue.go
@@ -0,0 +1,18 @@
+package event
+
+
+type Issue struct {
+	Title string
+	Description string
+	NodeId string
+	RepositoryUrl string
+	Url string
+	CommentsUrl string
+	HtmlUrl string
+	Id int64
+	Number int
+	State string
+	StateReason string
+	Locked bool
+	
+}
