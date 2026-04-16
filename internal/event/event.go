2aaf55a9c9133e5ad6368ca09774b7ddd55f6935
Sawsdevx8 <sawsdevx8@gmail.com>
2026-04-16 10:09:37 -0500

    Feat: Add event attributes

--- internal/event/event.go
+++ internal/event/event.go
@@ -3,5 +3,9 @@ package event
 type Event struct {
 	Id string `json:"id"`
 	Type string `json:"type"`
-	
+	Actor Actor `json:"actor"`
+	Repo Repo  `json:"repo"`
+	Payload Payload `json:"payload"`
+	Public bool `json:"public"`
+	CreatedAt string `json:"created_at"`
 }
